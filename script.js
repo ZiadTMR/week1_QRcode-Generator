63793084375ad5e0b8aa54f98b7542eaf0254be3
ZiadTMR <150849909+ZiadTMR@users.noreply.github.com>
2025-06-02 01:31:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const url = document.querySelector(".url");
+const generateBtn = document.querySelector(".generate");
+const display = document.querySelector(".display-URL");
+generateBtn.addEventListener("click", function() {
+    const websiteURL = url.value.trim();
+    if (websiteURL === '' || websiteURL !== '/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g' ) {
+        alert('Please enter a valid URL to continue.');
+        websiteURL = '';
+    }
+    else {
+        const encodedURL = encodeURIComponent(websiteURL);
+        const qrImageURL = `https://api.qrserver.com/v1/create-qr-code/?data=${encodedURL}&size=200x200`;
+        const qrImage = document.createElement('img');
+        const downloadBtn = document.createElement('button');
+        downloadBtn.classList.add('downloadBtn');
+        downloadBtn.textContent = 'Download';
+        qrImage.classList.add('qr-image');
+        qrImage.src = qrImageURL;
+        display.innerHTML = '';
+        display.appendChild(qrImage);
+        display.appendChild(downloadBtn);
+            downloadBtn.addEventListener("click", function() {
+            const link = document.createElement('a');
+            link.href = qrImageURL;
+            link.click();
+            link.download = 'qr-code.png';
+            });
+    }
+});
